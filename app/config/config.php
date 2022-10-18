fc8c3437c8faa6e728fcdf347a199a429e74c92a
Sander Dijkxhoorn <15117158+sanderDijkxhoorn@users.noreply.github.com>
2022-10-18 09:47:36 +0200

    app config and homepage

---
+++ app/config/config.php
@@ -0,0 +1,11 @@
+<?php
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', 'root');
+define('DB_NAME', 'mvc-oop-toets');
+
+define('APPROOT', dirname(dirname(__FILE__)));
+
+define('URLROOT', 'http://localhost:8888');
+
+define('SITENAME', 'MVC oop toets');
